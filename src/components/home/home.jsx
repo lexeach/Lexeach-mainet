763b5bce37b34fe0e9a763d5f21cbe3545d68321
lexeach <80155340+lexeach@users.noreply.github.com>
2023-12-13 07:54:26 +0530

    Update home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -268,7 +268,7 @@ const Dashboard = () => {
     }
 
     const chainParams = {
-      chainId: "0x56",
+      chainId: "0x38",
       chainName: "BNB Chain",
       nativeCurrency: {
         name: "BNB",
